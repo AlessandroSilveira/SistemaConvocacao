acb6e9159d120982c10e6c4629eb37d4136e12a0
Alessandro Silveira <alesilver.si@gmail.com>
2017-09-28 01:15:31 -0300

    commit

---
+++ src/SisConv.Infra.Data/Mapping/ClienteMap.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SisConv.Domain.Entities;
+using SisConv.Infra.Data.Extensions;
+
+namespace SisConv.Infra.Data.Mapping
+{
+    public class ClienteMap : EntityTypeConfiguration<Cliente>
+    {
+        public override void Map(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.HasKey(c => c.ClienteId);
+
+            builder.Property(c => c.ClienteId)
+                .HasColumnName("ClienteId");
+
+            builder.Property(c => c.Nome)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnType("varchar(100)");
+
+            builder.Property(c => c.Cnpj)
+                .IsRequired()
+                .HasMaxLength(15)
+                .HasColumnType("varchar(15)");
+
+            builder.Property(c => c.Email)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnType("varchar(100)");
+
+            builder.Property(c => c.Telefone)
+                .IsRequired()
+                .HasMaxLength(20)
+                .HasColumnType("varchar(20)");
+
+            builder.ToTable("Cliente");
+        }
+    }
+}
---
+++ src/SisConv.Infra.Data/Mapping/UsuarioMap.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SisConv.Domain.Entities;
+using SisConv.Infra.Data.Extensions;
+
+namespace SisConv.Infra.Data.Mapping
+{
+    public class UsuarioMap : EntityTypeConfiguration<Usuario>
+    {
+        public override void Map(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasKey(c => c.UsuarioId);
+
+            builder.Property(c => c.UsuarioId)
+                .HasColumnName("UsuarioId");
+
+            builder.Property(c => c.Nome)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnType("varchar(100)");
+
+            builder.Property(c => c.Email)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnType("varchar(100)");
+
+            builder.Property(c => c.Login)
+                .IsRequired()
+                .HasMaxLength(20)
+                .HasColumnType("varchar(20)");
+
+            builder.Property(c => c.Senha)
+                .IsRequired()
+                .HasMaxLength(10)
+                .HasColumnType("varchar(10)");
+
+            builder.Property(c => c.Perfil)
+                .IsRequired()
+                .HasMaxLength(1)
+                .HasColumnType("varchar(1)");
+
+            builder.Property(c => c.Ativo)
+                .IsRequired()
+                .HasMaxLength(1)
+                .HasColumnType("bool");
+        }
+    }
+}
