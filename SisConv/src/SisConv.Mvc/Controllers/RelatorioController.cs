285fd7be5956d2ecd36d5cbfae9ed69ff2189827
Alessandro Silveira <alesilver.si@gmail.com>
2018-02-19 22:29:09 -0300

    commit

--- SisConv/src/SisConv.Mvc/Controllers/RelatorioController.cs
+++ SisConv/src/SisConv.Mvc/Controllers/RelatorioController.cs
@@ -1,13 +1,48 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using SisConv.Application.Interfaces.Repository;
 
 namespace SisConv.Mvc.Controllers
 {
 	public class RelatorioController : Controller
-    {
-        // GET: Relatorio
-        public ActionResult Index()
+	{
+	    private readonly ICargoAppService _cargoAppService;
+	    private readonly IConvocadoAppService _convocadoAppService;
+	    private readonly IConvocacaoAppService _convocacaoAppService;
+
+	    public RelatorioController(ICargoAppService cargoAppService, IConvocadoAppService convocadoAppService, IConvocacaoAppService convocacaoAppService)
+	    {
+	        _cargoAppService = cargoAppService;
+	        _convocadoAppService = convocadoAppService;
+	        _convocacaoAppService = convocacaoAppService;
+	    }
+
+	    // GET: Relatorio
+        public ActionResult Index(Guid Id)
         {
+            ViewBag.ProcessoId = Id;
             return View();
         }
+
+        public ActionResult ClassificadosPorCargo(Guid id)
+        {
+            ViewBag.ProcessoId = id;
+            ViewBag.ListaCargos = _cargoAppService.Search(a => a.ProcessoId.Equals(id) && a.Ativo.Equals(true));
+            ViewBag.ListaConvocadosPorCargo = null;
+            return View();
+        }
+
+        [HttpPost]
+	    public ActionResult ClassificadosPorCargo(Guid id, Guid cargo)
+	    {
+	        ViewBag.ProcessoId = id;
+	        var dadosConfirmados = _convocacaoAppService.Search(a => a.ProcessoId.Equals(id)).Where(b => b.StatusConvocacao != null);
+	        var convocados = _convocadoAppService.Search(a => a.ProcessoId.Equals(id) && a.CargoId.Equals(cargo));
+	        ViewBag.ListaConvocadosPorCargo = _convocacaoAppService.MontaListaDeConvocados(dadosConfirmados, convocados);
+	        ViewBag.ListaCargos = _cargoAppService.Search(a => a.ProcessoId.Equals(id) && a.Ativo.Equals(true));
+
+            return View();
+	    }
     }
 }
