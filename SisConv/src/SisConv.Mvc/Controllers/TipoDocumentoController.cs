6cb6b6ed8b3dd131e23adbb8d04d95571be206e3
Silveira, Alessandro <alessandro.silveira@accenture.com>
2018-09-11 04:34:14 -0300

    Lista de Documentos

---
+++ SisConv/src/SisConv.Mvc/Controllers/TipoDocumentoController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace SisConv.Mvc.Controllers
+{
+    public class TipoDocumentoController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return
+            View();
+        }
+    }
+}
